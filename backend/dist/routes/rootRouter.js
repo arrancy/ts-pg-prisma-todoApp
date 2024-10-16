3acdb259d374625f318a29a233c4a165e9bcea30
arrancy <ruturajchondekar@gmail.com>
2024-10-16 18:22:00 +0530

    Stop tracking dist folder

--- backend/dist/routes/rootRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rootRouter = void 0;
-const express_1 = require("express");
-const UserRouter_1 = require("./user/UserRouter");
-exports.rootRouter = (0, express_1.Router)();
-exports.rootRouter.use("/user", UserRouter_1.userRouter);
